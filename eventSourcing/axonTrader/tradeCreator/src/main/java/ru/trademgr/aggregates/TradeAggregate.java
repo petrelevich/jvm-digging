00b88591837b1e2c1fd51dd6783c44204d092a5f
Sergey Petrelevich <petrelevich@yandex.ru>
2020-09-14 22:19:03 +0300

    axon demo

--- eventSourcing/axonTrader/tradeCreator/src/main/java/ru/trademgr/aggregates/TradeAggregate.java
+++ eventSourcing/axonTrader/tradeCreator/src/main/java/ru/trademgr/aggregates/TradeAggregate.java
@@ -3,7 +3,6 @@ package ru.trademgr.aggregates;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandHandler;
-import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
@@ -39,10 +38,5 @@ public class TradeAggregate {
                 .marketFee(command.marketFee())
                 .build();
         AggregateLifecycle.apply(event);
-    }
-
-    @EventSourcingHandler
-    public void on(CreatedTradeEvent event) {
-        log.info("on event: {}", event);
     }
 }
