7e530ebc7f40dda62e1fc11bc401928efb633f35
Sergey Petrelevich <petrelevich@yandex.ru>
2020-11-28 16:11:25 +0300

    LocalVars

---
+++ primitiveReferenceTypes/src/main/java/ru/petrelevich/bytes/LocalVars.java
@@ -0,0 +1,10 @@
+package ru.petrelevich.bytes;
+
+//javac -g LocalVars.java
+// javap -c -verbose LocalVars.class
+public class LocalVars {
+
+    public void eat(int piecesOfCheese) {
+        int bitesOfCheese = 1;
+    }
+}
