7d462d1aba1414a98dfa8d8cb9c7e8125028d82d
sergey <petrelevich@yandex.ru>
2021-06-11 10:37:20 +0300

    gc tune

---
+++ gcTuning/src/main/java/ru/calculator/Data.java
@@ -0,0 +1,13 @@
+package ru.calculator;
+
+public class Data {
+    private final Integer value;
+
+    public Data(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
