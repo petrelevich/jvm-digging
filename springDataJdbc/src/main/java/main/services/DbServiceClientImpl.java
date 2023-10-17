bd2b4d99558af6063147a4e6d1326ee36e92742a
petrelevich <petrelevich@yandex.ru>
2023-10-17 08:33:19 +0300

    updated spring data jdbc

--- springDataJdbc/src/main/java/main/services/DbServiceClientImpl.java
+++ springDataJdbc/src/main/java/main/services/DbServiceClientImpl.java
@@ -9,7 +9,6 @@ import main.transactions.TransactionRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DbServiceClientImpl implements DBServiceClient {
