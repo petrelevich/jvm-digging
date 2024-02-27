4722bdbb369a216b4ec84468b65ea17398c28b93
petrelevich <petrelevich@yandex.ru>
2024-02-27 22:15:02 +0300

    fixed IdeaLanguageLevel

--- springDataJdbc/build.gradle.kts
+++ springDataJdbc/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 idea {
     project {
-        languageLevel = IdeaLanguageLevel(17)
+        languageLevel = IdeaLanguageLevel(21)
     }
     module {
         isDownloadJavadoc = true
