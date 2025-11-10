670cb7d6cc8ec905aa0dafd683eb0b6eb43e52c2
petrelevich <petrelevich@yandex.ru>
2025-11-10 09:52:33 +0300

    новые примеры

--- virtual-thread/rest-service/runRestService.sh
+++ virtual-thread/rest-service/runRestService.sh
@@ -10,6 +10,6 @@ docker run --rm --name rest-service \
 --memory=256m \
 --cpus 1 \
 -p 8080:8080 \
--p 3000:3000 \
+-p 3001:3000 \
 -e JAVA_TOOL_OPTIONS="-XX:InitialRAMPercentage=80 -XX:MaxRAMPercentage=80 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=3000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false" \
 localrun/rest-service:latest
