ea013d5a845ee21662224f4dcdfd248477171388
petrelevich <petrelevich@yandex.ru>
2025-11-18 17:56:26 +0300

    новые примеры

--- virtual-thread/calculations-virtual-thread/runCalculationsVThread.sh
+++ virtual-thread/calculations-virtual-thread/runCalculationsVThread.sh
@@ -9,6 +9,6 @@ docker stop docker
 docker run --rm --name calculations-vthread \
 --memory=256m \
 --cpus 2 \
--p 3001:3000 \
+-p 3002:3000 \
 -e JAVA_TOOL_OPTIONS="-XX:InitialRAMPercentage=80 -XX:MaxRAMPercentage=80 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=3000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false" \
 localrun/calculations-vthread:latest
--- virtual-thread/calculations/runCalculations.sh
+++ virtual-thread/calculations/runCalculations.sh
@@ -9,6 +9,6 @@ docker stop docker
 docker run --rm --name calculations \
 --memory=256m \
 --cpus 2 \
--p 3000:3000 \
+-p 3001:3000 \
 -e JAVA_TOOL_OPTIONS="-XX:InitialRAMPercentage=80 -XX:MaxRAMPercentage=80 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=3000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false" \
 localrun/calculations:latest
