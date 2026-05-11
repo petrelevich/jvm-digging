0695a05c7aa7d5effa32057be9c739e05bc375e9
petrelevich <petrelevich@yandex.ru>
2026-05-11 17:08:09 +0300

    reactor-webflux

--- reactor-webflux/webflux-demo/src/main/java/ru/demo/spring/rest/PersonController.java
+++ reactor-webflux/webflux-demo/src/main/java/ru/demo/spring/rest/PersonController.java
@@ -1,10 +1,13 @@
 package ru.demo.spring.rest;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 import ru.demo.spring.domain.Notes;
 import ru.demo.spring.domain.Person;
 import ru.demo.spring.domain.PersonDto;
@@ -14,19 +17,23 @@ import ru.demo.spring.repository.PersonRepositoryCustom;
 
 @RestController
 public class PersonController {
-
+    private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
     private final PersonRepository personRepository;
     private final NotesRepository notesRepository;
 
     private final PersonRepositoryCustom personRepositoryCustom;
 
+    private final Scheduler workerPool;
+
     public PersonController(
             PersonRepository personRepository,
             NotesRepository notesRepository,
-            PersonRepositoryCustom personRepositoryCustom) {
+            PersonRepositoryCustom personRepositoryCustom,
+            Scheduler workerPool) {
         this.personRepository = personRepository;
         this.notesRepository = notesRepository;
         this.personRepositoryCustom = personRepositoryCustom;
+        this.workerPool = workerPool;
     }
 
     @GetMapping("/person")
@@ -36,15 +43,19 @@ public class PersonController {
 
     @GetMapping("/person/{id}")
     public Mono<ResponseEntity<PersonDto>> byId(@PathVariable("id") Long id) {
+        logger.info("Get person by id {}", id);
         return personRepository
                 .findById(id)
+                .publishOn(workerPool)
                 .flatMap(person -> notesRepository
                         .findByPersonId(person.getId())
+                        .publishOn(workerPool)
                         .map(Notes::getNoteText)
                         .collectList()
                         .map(notes -> toDto(person, notes)))
                 .map(ResponseEntity::ok)
-                .switchIfEmpty(Mono.fromCallable(() -> ResponseEntity.notFound().build()));
+                .switchIfEmpty(Mono.fromCallable(() -> ResponseEntity.notFound().build()))
+                .subscribeOn(workerPool);
     }
 
     @PostMapping("/person")
@@ -58,6 +69,7 @@ public class PersonController {
     }
 
     private PersonDto toDto(Person person, List<String> notes) {
+        logger.info("person={}", person);
         return new PersonDto(String.valueOf(person.getId()), person.getLastName(), person.getAge(), notes);
     }
 }
